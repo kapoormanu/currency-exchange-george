ef3e557367863e5b70a1bc8f1972855a940be938
Manu Kapoor <daak@manukapoor.in>
2022-01-20 20:12:05 +0530

    Feat: i18n: add locale detection and basic i18n file for DE

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,13 +1,23 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 import { I18N_EN } from './en/en.i18n';
+import { I18N_DE } from './de/de.i18n';
 
-i18n.use(initReactI18next).init({
-    resources: {
-        en: {
-            translation: I18N_EN
-        }
-    },
-    fallbackLng: 'en'
-});
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: {
+                translation: I18N_EN
+            },
+            'de-AT': {
+                translation: I18N_DE
+            },
+            'de-DE': {
+                translation: I18N_DE
+            }
+        },
+        fallbackLng: 'en'
+    });
