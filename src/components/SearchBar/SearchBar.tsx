9adc8844e33391ffc86dcd5effe6bbc067b5b27f
Manu Kapoor <daak@manukapoor.in>
2022-01-20 18:30:20 +0530

    Docs: Header_SearchBar: add docs to components and functions

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -5,9 +5,26 @@ export type SearchBarProps = {
     searchTerm?: string;
 };
 
+/**
+ * SearchBar Component. Includes a searchbox with an associated label.
+ * Accepts value from props to populate the searchbox or initializes it with ''.
+ *
+ * @component
+ * @param {SearchBarProps} props the props passed to the component
+ * @property {string} props.searchTerm (Optional) value to set in searchbox
+ * @returns {JSX.Element} a JSX element composing the SearchBar
+ */
 function SearchBar(props: SearchBarProps) {
     const [searchField, setSearchField] = React.useState(props.searchTerm || '');
 
+    /**
+     * Function invoked on searchbox input change. Updates the state value.
+     *
+     * @function
+     * @private
+     * @param {React.ChangeEvent<HTMLInputElement>} e the event created on input change
+     * @returns {Void} nothing
+     */
     const handleSearchFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { value } = e.target;
         setSearchField(value);
