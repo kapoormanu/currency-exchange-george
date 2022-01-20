077a0f628a3460b4f8d8be3eee3c19326fd9fad5
Manu Kapoor <daak@manukapoor.in>
2022-01-20 20:20:43 +0530

    Fix: i18n: move config into a separate file

---
+++ src/i18n/i18n.config.js
@@ -0,0 +1,15 @@
+import { I18N_EN } from './en/en.i18n';
+import { I18N_DE } from './de/de.i18n';
+
+const i18nConfig = {
+    en: {
+        translation: I18N_EN
+    },
+    'de-AT': {
+        translation: I18N_DE
+    },
+    'de-DE': {
+        translation: I18N_DE
+    }
+};
+export default i18nConfig;
