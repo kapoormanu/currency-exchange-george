da9c48362c6d7091033996d8f3c3c8fd1e5e8bf3
Manu Kapoor <daak@manukapoor.in>
2022-02-02 19:40:49 +0530

    Test: CurrencyUtilFunctions: add specs and empty currency utility functions

---
+++ src/utils/currency.ts
@@ -0,0 +1,9 @@
+const getFormattedExchangeRate = (exchangeRate: string, baseCurrency: string, minPrecision: number = 0) => '';
+const getCountryFromCurrency = (currency: string) => '';
+
+const utils = {
+    getFormattedExchangeRate,
+    getCountryFromCurrency
+};
+
+export default utils;
