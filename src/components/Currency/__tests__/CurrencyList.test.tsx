fedc2d3d7640447eb9701b64c89defe078ae0e85
Manu Kapoor <daak@manukapoor.in>
2022-02-05 11:03:35 +0530

    Test: CurrencyList: Add more tests, abstract away render

--- src/components/Currency/__tests__/CurrencyList.test.tsx
+++ src/components/Currency/__tests__/CurrencyList.test.tsx
@@ -3,31 +3,42 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import CurrencyList from 'components/Currency/CurrencyList';
 import fxData from 'mocks/data/fx.json';
+import { rest } from 'msw';
+import { server } from 'mocks/mockServer';
 
+const renderCurrencyList = async () => {
+    const utils = render(<CurrencyList />);
+    // Show loading currencies while fetching from API
+    expect(await screen.findByText('Loading Currencies...')).toBeInTheDocument();
+    await waitFor(() => {
+        // and then hide it when loaded
+        expect(screen.queryByText('Loading Currencies...')).not.toBeInTheDocument();
+    });
+    return utils;
+};
 describe('<CurrencyList/>', () => {
     it('should load all the countries from API', async () => {
-        render(<CurrencyList />);
-
-        // Show loading currencies while fetching from API
-        expect(await screen.findByText('Loading Currencies...')).toBeInTheDocument();
-        await waitFor(() => {
-            // and then hide it when loaded
-            expect(screen.queryByText('Loading Currencies...')).not.toBeInTheDocument();
-        });
+        await renderCurrencyList();
         expect(screen.getAllByRole('row')).toHaveLength(fxData.fx.length);
     });
 
     it('should load flags for all countries including placeholders for missing flags', async () => {
-        render(<CurrencyList />);
-
-        // Show loading currencies while fetching from API
-        expect(await screen.findByText('Loading Currencies...')).toBeInTheDocument();
-        await waitFor(() => {
-            // and then hide it when loaded
-            expect(screen.queryByText('Loading Currencies...')).not.toBeInTheDocument();
-        });
+        await renderCurrencyList();
 
         // Assumption: no other images are rendered inside CurrencyList
         expect(screen.getAllByRole('img')).toHaveLength(fxData.fx.length);
+    });
+
+    it('should show an error message if API call failed', async () => {
+        server.resetHandlers(
+            rest.get('https://run.mocky.io/v3/c88db14a-3128-4fbd-af74-1371c5bb0343', (req, resp, ctx) => {
+                return resp(ctx.status(400, 'Server Error'));
+            })
+        );
+        await renderCurrencyList();
+        // TODO: Improve this test
+        const alert = screen.getByRole('alert');
+        expect(alert).toBeInTheDocument();
+        expect(alert).toHaveTextContent('Oops. There was an Error fetching currencies.');
     });
 });
