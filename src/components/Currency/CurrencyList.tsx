8731bd3cd3d5088216da93faca2e0ccb99101cdc
Manu Kapoor <daak@manukapoor.in>
2022-02-05 14:54:50 +0530

    Feat: CurrencyList: Complete currency list implementation

--- src/components/Currency/CurrencyList.tsx
+++ src/components/Currency/CurrencyList.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useAppDispatch, useAppSelector } from 'app/hooks';
+import CurrencyItem from 'components/Currency/CurrencyItem';
+import { apiState } from 'types/global';
+import { fetchCurrencies } from 'components/Currency/currencySlice';
+import { getAllCurrencies, getBaseCurrency, getLoadingStatus } from 'components/Currency/currencySelector';
 
 function CurrencyList() {
-    return <></>;
+    const dispatch = useAppDispatch();
+    const { state: loadingStatus } = useAppSelector(getLoadingStatus);
+    const currencies = useAppSelector(getAllCurrencies);
+    const baseCurrency = useAppSelector(getBaseCurrency);
+    const currencyItems = currencies.length ? (
+        currencies.map((currency) => (
+            // React gives a false warning about this line that this shouldn't be wrapped in a <div>
+            <CurrencyItem key={currency.currency} baseCurrency={baseCurrency} currencyData={currency} />
+        ))
+    ) : (
+        <></>
+    );
+    useEffect(() => {
+        if (loadingStatus === apiState.PRISTINE) {
+            dispatch(fetchCurrencies());
+        }
+    }, [loadingStatus, dispatch]);
+
+    return (
+        <>
+            {loadingStatus === apiState.LOADING && <div>Loading Currencies...</div>}
+            {loadingStatus === apiState.FAILURE && (
+                <div role='alert' className='fade alert alert-danger show'>
+                    Oops. There was an Error fetching currencies.
+                </div>
+            )}
+            {loadingStatus === apiState.SUCCESS && currencies.length && (
+                <table>
+                    <thead>
+                        <tr>
+                            <th id='flag'>Flag</th>
+                            <th id='currency'>Currency</th>
+                            <th id='country'>Country</th>
+                            <th id='buy'>Buy</th>
+                            <th id='sell'>Sell</th>
+                        </tr>
+                    </thead>
+                    <tbody>{currencyItems}</tbody>
+                </table>
+            )}
+            {loadingStatus === apiState.SUCCESS && !currencies.length && (
+                <div role='alert' className='fade alert alert-danger show'>
+                    No currencies available.
+                </div>
+            )}
+        </>
+    );
 }
 
 export default CurrencyList;
