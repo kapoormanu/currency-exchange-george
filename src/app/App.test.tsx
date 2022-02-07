2269ecd376564f11884cef1dc5a87b7b2f45da67
Manu Kapoor <daak@manukapoor.in>
2022-02-08 00:47:39 +0530

    Refactor: App.test: add comments, rearrange to distinguish sections of tests

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -17,51 +17,63 @@ describe('<App />', () => {
     it('renders app', () => {
         setupApp();
     });
+
     it('renders the Header successfully', async () => {
         setupApp();
+
         const heading = await screen.findByRole('heading');
+
         expect(heading).toBeInTheDocument();
     });
+
     it('renders the searchbar successfully', async () => {
         setupApp();
+
         const searchbox = await screen.findByRole('searchbox');
+
         expect(searchbox).toBeInTheDocument();
     });
 
     it('renders the currencylist successfully', async () => {
         setupApp();
+
         const currencyList = await screen.findByRole('table', { name: 'Currency List' });
+
         expect(currencyList).toBeInTheDocument();
     });
 
     it('should show only results matching the search item', async () => {
         const searchTerm = 'me';
-
         setupApp();
         const currencyList = await screen.findByRole('table', { name: 'Currency List' });
         const searchBox = screen.getByRole('searchbox');
-        expect(searchBox).toHaveValue('');
+
+        // Type the searchterm to filter results
         userEvent.type(searchBox, searchTerm);
         const unmatchedItem: HTMLElement | null = within(currencyList).queryByRole('cell', { name: 'FJD' });
-        expect(unmatchedItem).not.toBeInTheDocument();
         const matchedItem: HTMLElement | null = within(currencyList).queryByRole('img', { name: 'Mexican Peso' });
+
+        // Check that only expected items are present due to filter term
+        expect(unmatchedItem).not.toBeInTheDocument();
         expect(matchedItem).toBeInTheDocument();
     });
 
     it('should revert to show all currencies on clearing the search input', async () => {
         const searchTerm = 'me';
-        let matchedItem: HTMLElement | null;
         setupApp();
         const currencyList = await screen.findByRole('table', { name: 'Currency List' });
         const searchBox = screen.getByRole('searchbox');
-
         userEvent.clear(searchBox);
+        // populate with search term
         userEvent.type(searchBox, searchTerm);
-        expect(searchBox).toHaveValue(searchTerm);
-        matchedItem = within(currencyList).queryByRole('cell', { name: 'FJD' });
-        expect(matchedItem).not.toBeInTheDocument();
+
+        // Check that non-matching element should not be there
+        const unmatchedItem = within(currencyList).queryByRole('cell', { name: 'FJD' });
+        expect(unmatchedItem).not.toBeInTheDocument();
+
+        // Clear searchbox and then check the previously missing item should now be present
         userEvent.clear(searchBox);
-        matchedItem = within(currencyList).getByRole('cell', { name: 'FJD' });
+        const matchedItem = within(currencyList).getByRole('cell', { name: 'FJD' });
         expect(matchedItem).toBeInTheDocument();
     });
 });
