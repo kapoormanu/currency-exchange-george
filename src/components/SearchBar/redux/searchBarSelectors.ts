eabcfe3424052fb350f9bfec77fdd7a0a81e03d9
Manu Kapoor <daak@manukapoor.in>
2022-02-08 02:56:02 +0530

    Refactor: store: move store from app to app/redux

--- src/components/SearchBar/redux/searchBarSelectors.ts
+++ src/components/SearchBar/redux/searchBarSelectors.ts
@@ -1,4 +1,4 @@
-import { RootState } from 'app/store';
+import { RootState } from 'app/redux/store';
 
 // get the search slice to use in SearchBar
 export function searchSelector(state: RootState) {
