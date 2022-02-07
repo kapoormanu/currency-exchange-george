6cf423f16c38b3e3f1175ccaa15f2338d7027eca
Manu Kapoor <daak@manukapoor.in>
2022-02-08 00:49:38 +0530

    Fix: Header.test: minor spacing and test name change

--- src/components/Header/__tests__/Header.test.tsx
+++ src/components/Header/__tests__/Header.test.tsx
@@ -4,8 +4,9 @@ import { render, screen } from '@testing-library/react';
 import Header from 'components/Header/Header';
 
 describe('<Header/ >', () => {
-    it('should have the header text', () => {
+    it('should have the header i18n key', () => {
         render(<Header />);
+
         expect(screen.getByRole('heading')).toHaveTextContent('header.title');
     });
 });
