688ec1cb4f2116669e0e536b3311830922977677
Manu Kapoor <daak@manukapoor.in>
2022-02-08 01:46:32 +0530

    Refactor: handlers: use const for URL

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,18 @@
 import { rest } from 'msw';
 import fxData from 'mocks/data/fx.json';
-const getCurrenciesUrl = 'https://run.mocky.io/v3/c88db14a-3128-4fbd-af74-1371c5bb0343';
-export const handlers = [
-    rest.get(getCurrenciesUrl, (req, resp, ctx) => {
+import { GET_CURRENCIES_ENDPOINT } from 'http/endpoints';
+
+const defaultHandler = () => {
+    return rest.get(GET_CURRENCIES_ENDPOINT, (req, resp, ctx) => {
         return resp(ctx.json(fxData));
-    })
-];
-export const currencyHandlerException = rest.get(getCurrenciesUrl, async (req, res, ctx) =>
+    });
+};
+
+export const currencyHandlerException = rest.get(GET_CURRENCIES_ENDPOINT, async (req, res, ctx) =>
     res(ctx.status(500), ctx.json({ message: 'Deliberately broken request' }))
 );
-export const currencyHandlerEmpty = rest.get(getCurrenciesUrl, async (req, res, ctx) => {
+export const currencyHandlerEmpty = rest.get(GET_CURRENCIES_ENDPOINT, async (req, res, ctx) => {
     return res(ctx.json({ ...fxData, fx: [] }));
 });
+
+export const handlers = [defaultHandler()];
