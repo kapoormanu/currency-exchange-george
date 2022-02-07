a8df07fd7b623b7fb3aac01e4a0a1f83178db1dd
Manu Kapoor <daak@manukapoor.in>
2022-02-08 00:16:27 +0530

    Test: currencyService: test implementation

---
+++ src/http/__tests__/GetCurrencyService.test.ts
@@ -0,0 +1,42 @@
+import { server } from 'mocks/mockServer';
+import { currencyHandlerEmpty, currencyHandlerException } from 'mocks/handlers';
+import fxData from 'mocks/data/fx.json';
+
+import { CurrencyService } from 'http/services/GetCurrencyService';
+import { AxiosHttpClient } from 'http/clients/AxiosHttpClient';
+import { currenciesResponseData } from 'http/currenciesResponse';
+
+let getCurrencies: ReturnType<typeof CurrencyService>;
+
+beforeEach(() => {
+    // Inject with client of choice, preferably same as used elsewhere in the app.
+    getCurrencies = CurrencyService(AxiosHttpClient());
+});
+
+describe('currenciesApi', () => {
+    it('should return fx data when API runs successfully', async () => {
+        const response = await getCurrencies();
+
+        expect(response.fx).toHaveProperty('fx', fxData.fx);
+    });
+    it('should return failure when API returns 500 status', async () => {
+        server.use(currencyHandlerException);
+        const Error500 = new Error('Request failed with status code 500');
+
+        let response;
+        try {
+            response = await getCurrencies();
+        } catch (err) {
+            response = err;
+        }
+
+        expect(response).toEqual(Error500);
+    });
+    it('should return empty fx array when API response doesnt have data', async () => {
+        server.use(currencyHandlerEmpty);
+
+        const response: currenciesResponseData = await getCurrencies();
+
+        expect(response.fx).toEqual([]);
+    });
+});
