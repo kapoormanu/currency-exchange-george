746e0c0b7acc90eb9fac44b9d26cc1760af8450c
Manu Kapoor <daak@manukapoor.in>
2022-02-08 09:12:21 +0530

    Test: Input: introduce specs for Input component

---
+++ src/components/UI/Input/Input.tsx
@@ -0,0 +1,10 @@
+import React, { InputHTMLAttributes } from 'react';
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+    label?: string;
+};
+function Input(props: InputProps) {
+    return <></>;
+}
+
+export default Input;
