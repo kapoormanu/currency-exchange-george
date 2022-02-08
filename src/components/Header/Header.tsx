54ac20d89adc1ba5764640adbdd1e19270d8962e
Manu Kapoor <daak@manukapoor.in>
2022-02-08 16:39:47 +0530

    Feat: Header: use MUI components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Trans } from 'react-i18next';
 
-import styles from 'components/Header/Header.module.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import styles from 'components/Header/Header.module.scss';
 
 /**
  * The Header component used to show the website header using the <header> tag.
@@ -13,9 +17,15 @@ import styles from 'components/Header/Header.module.css';
 function Header() {
     return (
         <header className={styles.header}>
-            <h1 className={styles.headerTitle}>
-                <Trans>header.title</Trans>
-            </h1>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position='static' className={styles.headerTitle}>
+                    <Toolbar>
+                        <Typography variant='h6' component='h1' sx={{ flexGrow: 1 }}>
+                            <Trans>header.title</Trans>
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
         </header>
     );
 }
