90246a328fd29e82c535f02f749eec599b9bd33e
Manu Kapoor <daak@manukapoor.in>
2022-02-08 12:57:49 +0530

    Feat: CurrenciesList: compose display using UI component CurrenciesDisplay.tsx

--- src/components/Currency/CurrenciesList.tsx
+++ src/components/Currency/CurrenciesList.tsx
@@ -1,50 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useAppDispatch, useAppSelector } from 'app/redux/hooks';
-import { apiState } from 'types/global';
-
-import CurrencyItem from 'components/Currency/CurrencyItem';
-import {
-    getBaseCurrency,
-    getFilteredCurrencies,
-    getLoadingStatus
-} from 'components/Currency/redux/currenciesDataSelector';
-import { currenciesActions, fetchCurrencies } from 'components/Currency/redux/currenciesDataSlice';
-
-import { searchSelector } from 'components/SearchBar/redux/searchBarSelectors';
+import { useGetCurrenciesListData } from './redux/useGetCurrenciesListData';
+import CurrenciesDisplay from './UI/CurrenciesDisplay';
 
 function CurrenciesList() {
     const { loadingStatus, filteredCurrencies, currencyItems } = useGetCurrenciesListData();
     return (
         <>
-            {loadingStatus === apiState.LOADING && <div>Loading Currencies...</div>}
-            {loadingStatus === apiState.FAILURE && (
-                <div role='alert' className='fade alert alert-danger show'>
-                    Oops. There was an Error fetching currencies.
-                </div>
-            )}
-            {loadingStatus === apiState.SUCCESS && (
-                <table>
-                    <caption id='currency-list'>Currency List</caption>
-                    <thead>
-                        <tr>
-                            <th id='flag'>Flag</th>
-                            <th id='currency'>Currency</th>
-                            <th id='country'>Country</th>
-                            <th id='buy'>Buy</th>
-                            <th id='sell'>Sell</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {filteredCurrencies.length > 0 && currencyItems}
-                        {filteredCurrencies.length === 0 && (
-                            <div role='alert' className='fade alert alert-danger show'>
-                                No currencies available.
-                            </div>
-                        )}
-                    </tbody>
-                </table>
-            )}
+            <CurrenciesDisplay
+                loadingStatus={loadingStatus}
+                filteredCurrencies={filteredCurrencies}
+                currencyItems={currencyItems}
+            />
         </>
     );
 }
---
+++ src/components/Currency/UI/CurrenciesDisplay.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Currency } from 'types/currency';
+import { apiState } from 'types/global';
+
+type CurrenciesDisplayProps = {
+    loadingStatus: apiState;
+    filteredCurrencies: Currency[];
+    currencyItems: JSX.Element | JSX.Element[];
+};
+const CurrenciesDisplay = (props: CurrenciesDisplayProps) => {
+    return (
+        <>
+            {props.loadingStatus === apiState.LOADING && <div>Loading Currencies...</div>}
+            {props.loadingStatus === apiState.FAILURE && (
+                <div role='alert' className='fade alert alert-danger show'>
+                    Oops. There was an Error fetching currencies.
+                </div>
+            )}
+            {props.loadingStatus === apiState.SUCCESS && (
+                <table>
+                    <caption id='currency-list'>Currency List</caption>
+                    <thead>
+                        <tr>
+                            <th id='flag'>Flag</th>
+                            <th id='currency'>Currency</th>
+                            <th id='country'>Country</th>
+                            <th id='buy'>Buy</th>
+                            <th id='sell'>Sell</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.filteredCurrencies.length > 0 && props.currencyItems}
+                        {props.filteredCurrencies.length === 0 && (
+                            <div role='alert' className='fade alert alert-danger show'>
+                                No currencies available.
+                            </div>
+                        )}
+                    </tbody>
+                </table>
+            )}
+        </>
+    );
+};
+
+export default CurrenciesDisplay;
